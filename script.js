44f589cec0dac1f8512032ada634e3737d756676
earthameth <arthameth.tangkittipol@gmail.com>
2026-03-09 15:46:47 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -15,22 +15,26 @@ var myIcon = L.icon({
 });
 
 
-// Grand Palace
+// 1 Grand Palace
 var marker1 = L.marker([13.7515,100.4926],{icon:myIcon}).addTo(map);
-marker1.bindPopup("<b>Grand Palace</b><br>Bangkok<br><img src='https://upload.wikimedia.org/wikipedia/commons/1/10/Grand_Palace_Bangkok.jpg' width='150'>");
+marker1.bindPopup("<b>Grand Palace</b><br>Phra Nakhon, Bangkok<br><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Grand_Palace_Bangkok.jpg/500px-Grand_Palace_Bangkok.jpg' width='150'>");
 
-// Wat Phra Kaew
+
+// 2 Wat Phra Kaew
 var marker2 = L.marker([13.7528,100.4940],{icon:myIcon}).addTo(map);
-marker2.bindPopup("<b>Wat Phra Kaew</b><br>Bangkok<br><img src='https://upload.wikimedia.org/wikipedia/commons/c/c1/Wat_Phra_Kaew.jpg' width='150'>");
+marker2.bindPopup("<b>Wat Phra Kaew</b><br>Phra Nakhon, Bangkok<br><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Wat_Phra_Kaew_by_Ninara_TSP_edit_crop.jpg/500px-Wat_Phra_Kaew_by_Ninara_TSP_edit_crop.jpg' width='150'>");
 
-// Lumphini Park
+
+// 3 Lumphini Park
 var marker3 = L.marker([13.7308,100.5418],{icon:myIcon}).addTo(map);
-marker3.bindPopup("<b>Lumphini Park</b><br>Bangkok<br><img src='https://upload.wikimedia.org/wikipedia/commons/3/3b/Lumphini_Park_Bangkok.jpg' width='150'>");
+marker3.bindPopup("<b>Lumphini Park</b><br>Bangkok<br><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Lumphini_Park_Bangkok.jpg/500px-Lumphini_Park_Bangkok.jpg' width='150'>");
 
-// Siam Paragon
+
+// 4 Siam Paragon
 var marker4 = L.marker([13.7465,100.5347],{icon:myIcon}).addTo(map);
-marker4.bindPopup("<b>Siam Paragon</b><br>Bangkok<br><img src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Siam_Paragon.jpg' width='150'>");
+marker4.bindPopup("<b>Siam Paragon</b><br>Pathum Wan, Bangkok<br><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Siam_Paragon.jpg/500px-Siam_Paragon.jpg' width='150'>");
 
-// Wat Arun
+
+// 5 Wat Arun
 var marker5 = L.marker([13.7437,100.4889],{icon:myIcon}).addTo(map);
-marker5.bindPopup("<b>Wat Arun</b><br>Bangkok<br><img src='https://upload.wikimedia.org/wikipedia/commons/3/3e/Wat_Arun_Ratchawararam.jpg' width='150'>");
+marker5.bindPopup("<b>Wat Arun</b><br>Bangkok Yai, Bangkok<br><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Wat_Arun_Ratchawararam.jpg/500px-Wat_Arun_Ratchawararam.jpg' width='150'>");
